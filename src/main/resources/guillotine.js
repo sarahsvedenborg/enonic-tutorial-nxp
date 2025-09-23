1b18f32e58adaf7240fcf94a0bba8a61271db809
Sarah Svedenborg <sarah.svedenborg@soprasteria.com>
2025-09-23 09:54:04 +0200

    more content types

---
+++ src/main/resources/guillotine.js
@@ -0,0 +1,28 @@
+exports.extensions = function (graphQL) {
+    return {
+        enums: {
+            // enum type definitions ...
+        },
+        inputTypes: {
+            // input type definitions ...
+        },
+        interfaces: {
+            // interfaces type definitions ...
+        },
+        unions: {
+            // unions type definitions ...
+        },
+        types: {
+            // output types definitions ...
+        },
+        creationCallbacks: {
+            // creation callback definitions ...
+        },
+        resolvers: {
+            // resolver definitions ...
+        },
+        typeResolvers: {
+            // type resolver definitions ...
+        }
+    }
+};
